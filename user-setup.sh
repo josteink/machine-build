31538c7e7d98c2cfaebfdacc47ba80beba18839f
Jostein Kjønigsen <jostein@kjonigsen.net>
2013-08-15 13:46:44 +0200

    Add git config. Permissions.

---
+++ user-setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+source includes.sh
+
+# setup git and user-defaults.
+
+git config --global user.name "Jostein Kjønigsen"
+git config --global user.email jostein@kjonigsen.net
+
