50a8bced294181bb15f178daa78d3d7628e306a6
Jostein Kjønigsen <jostein@kjonigsen.net>
2013-08-15 13:56:59 +0200

    Add (preliminary) Skype installation code.

---
+++ third-party/skype_install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source ../includes.sh
+
+$PREVALIDATE_ROOT
+
+# URL was obtained by visiting the following webpage and choosing "dynamic" as distribution type.
+# http://www.skype.com/en/download-skype/skype-for-linux/
+URL="http://download.skype.com/linux/skype-4.2.0.11.tar.bz2"
+
+# TODO: wget this
+# TODO: unpack to /usr/local/bin/
+# TODO: copy application shortcur to /usr/share/applications
+
