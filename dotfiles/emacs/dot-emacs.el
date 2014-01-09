f25b426037317a1f6a1382ee13a71fc92a13fbf6
Jostein Kjønigsen <jostein@kjonigsen.net>
2014-01-09 22:22:26 +0100

    Fix broken dead-keys in X on Linux.

--- dotfiles/emacs/dot-emacs.el
+++ dotfiles/emacs/dot-emacs.el
@@ -541,3 +541,21 @@ point reaches the beginning or end of the buffer, stop there."
 (if (display-graphic-p)
     (my-gui-mode-hook)
   'nothing)
+
+(defun my-x-mode-hook ()
+  (define-key key-translation-map [dead-grave] (lookup-key key-translation-map "\C-x8`"))
+  (define-key key-translation-map [dead-acute] (lookup-key key-translation-map "\C-x8'"))
+  (define-key key-translation-map [dead-circumflex] (lookup-key key-translation-map "\C-x8^"))
+  (define-key key-translation-map [dead-diaeresis] (lookup-key key-translation-map "\C-x8\""))
+  (define-key key-translation-map [dead-tilde] (lookup-key key-translation-map "\C-x8~"))
+  (define-key isearch-mode-map [dead-grave] nil)
+  (define-key isearch-mode-map [dead-acute] nil)
+  (define-key isearch-mode-map [dead-circumflex] nil)
+  (define-key isearch-mode-map [dead-diaeresis] nil)
+  (define-key isearch-mode-map [dead-tilde] nil))
+
+;; special workaround for dead keys needed only when running emacs in Linux & X.
+(if (and (display-graphic-p)
+         (eq system-type 'gnu/linux)
+         (my-x-mode-hook))
+    'nothing)
