d066e8e1abcf3b566e1724ac9b1b3d235e40df8f
Jostein Kjønigsen <jostein@kjonigsen.net>
2014-01-15 22:10:53 -0800

    Bugfix.

--- dotfiles/emacs/dot-emacs.el
+++ dotfiles/emacs/dot-emacs.el
@@ -86,7 +86,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- 'auto-revert-mode t
+ '(global-auto-revert-mode t)
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
