1b080852dbf5a47edd73f5d26a113db4858d756e
Jostein Kjønigsen <jostein@kjonigsen.net>
2014-03-20 11:51:23 +0100

    Clean up modelist.

--- dotfiles/emacs/dot-emacs.el
+++ dotfiles/emacs/dot-emacs.el
@@ -140,17 +140,21 @@
 (dolist (command '(yank yank-pop))
   (eval `(defadvice ,command (after indent-region activate)
            (and (not current-prefix-arg)
-                (member major-mode '(emacs-lisp-mode lisp-mode  clojure-mode scheme-mode
-                                                     haskell-mode
-                                                     ruby-mode
-                                                     rspec-mode
-                                                     python-mode
-                                                     c-mode
-                                                     c++-mode
-                                                     objc-mode
-                                                     latex-mode
-                                                     plain-tex-mode
-                                                     prog-mode)) ; sadly setting prog-mode alone is not enough
+                (member major-mode
+                        '(emacs-lisp-mode
+                          lisp-mode
+                          clojure-mode
+                          scheme-mode
+                          haskell-mode
+                          ruby-mode
+                          rspec-mode
+                          python-mode
+                          c-mode
+                          c++-mode
+                          objc-mode
+                          latex-mode
+                          plain-tex-mode
+                          prog-mode)) ; sadly setting prog-mode alone is not enough
                 (let
                     ((mark-even-if-inactive transient-mark-mode))
                   (indent-region (region-beginning)
