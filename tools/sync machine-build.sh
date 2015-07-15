e3eae3c93efc5376d522c8ee0e69416c293c531e
Jostein Kjønigsen <jostein@kjonigsen.net>
2015-07-15 09:34:28 +0200

    Cygwin script: Don't force rebase.

--- tools/sync machine-build.sh
+++ tools/sync machine-build.sh
@@ -5,5 +5,4 @@ CURRENT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$CURRENT/.."
 
 cd $ROOT
-# rebase to avoid getting merge-conflicts if we can avoid it!
-git pull --rebase
+git pull
