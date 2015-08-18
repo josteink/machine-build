d025622fdb96e9d5d84c10007bc138d491795c2d
Jostein Kjønigsen <jostein@kjonigsen.net>
2015-08-18 23:23:45 +0200

    Improve Win32 Emacs bootstrapper.
    Add gif & jpg graphics-libraries to emacs bootstrap process.
    Should improve eww experience.

--- tools/emacs-win32-bootstrap.cmd
+++ tools/emacs-win32-bootstrap.cmd
@@ -17,26 +17,21 @@ curl -O "http://ftp.gnu.org/gnu/emacs/windows/emacs-24.5-bin-i686-mingw32.zip"
 
 echo Getting ezwinports (source http://sourceforge.net/projects/ezwinports/files/)...
 
-echo - gnutls
 curl -O "http://skylink.dl.sourceforge.net/project/ezwinports/gnutls-3.3.11-w32-bin.zip"
-
-echo - libpng
+curl -O "http://skylink.dl.sourceforge.net/project/ezwinports/giflib-5.1.0-w32-bin.zip"
+curl -O "http://skylink.dl.sourceforge.net/project/ezwinports/jpeg-v9a-w32-bin.zip"
 curl -O "http://skylink.dl.sourceforge.net/project/ezwinports/libpng-1.6.12-w32-bin.zip"
-
-echo - libsvg
-curl -O "http://vorboss.dl.sourceforge.net/project/ezwinports/librsvg-2.40.1-2-w32-bin.zip"
-
-echo - libxml
-curl -O "http://kent.dl.sourceforge.net/project/ezwinports/libxml2-2.7.8-w32-bin.zip"
-
-echo - zlib
-curl -O "http://netassist.dl.sourceforge.net/project/ezwinports/zlib-1.2.8-2-w32-bin.zip"
+curl -O "http://skylink.dl.sourceforge.net/project/ezwinports/librsvg-2.40.1-2-w32-bin.zip"
+curl -O "http://skylink.dl.sourceforge.net/project/ezwinports/libxml2-2.7.8-w32-bin.zip"
+curl -O "http://skylink.dl.sourceforge.net/project/ezwinports/zlib-1.2.8-2-w32-bin.zip"
 
 echo Unpacking...
 cd ..
 
 unzip download/emacs-24.5-bin-i686-mingw32.zip
 unzip -o download/gnutls-3.3.11-w32-bin.zip
+unzip -o download/giflib-5.1.0-w32-bin.zip
+unzip -o download/jpeg-v9a-w32-bin.zip
 unzip -o download/libpng-1.6.12-w32-bin.zip
 unzip -o download/librsvg-2.40.1-2-w32-bin.zip
 unzip -o download/libxml2-2.7.8-w32-bin.zip
