c767f64706923be1e9bf7f9bb75742dcd1835716
Jostein Kjønigsen <jostein@kjonigsen.net>
2015-08-31 21:42:34 +0200

    Emacs: Better spell-checking in all text-modes.
    Add langtool to handle grammar.

--- dotfiles/emacs/dot-emacs.el
+++ dotfiles/emacs/dot-emacs.el
@@ -813,6 +813,18 @@ With a prefix argument N, (un)comment that many sexps."
 ;; indent properly, always
 (gsk 'newline-and-indent "RET")
 
+
+;; spell-checking via languagetool
+(let* ((jar (expand-file-name "~/build/LanguageTool-3.0/languagetool-commandline.jar")))
+  (when (file-exists-p jar)
+    (require 'langtool)
+    (setq langtool-language-tool-jar jar
+          langtool-mother-tongue "en"
+          langtool-disabled-rules '("WHITESPACE_RULE"
+                                    "EN_UNPAIRED_BRACKETS"
+                                    "COMMA_PARENTHESIS_WHITESPACE"
+                                    "EN_QUOTES"))))
+
 ;; prevent accidentally enabling overwrite-mode
 (defun overwrite-mode-prompt ()
   "A wrapper to ensure overwrite-mode is never enabled blindly."
@@ -1068,9 +1080,17 @@ With a prefix argument N, (un)comment that many sexps."
 ;; css should be prog-mode but isn't
 (add-hook 'css-mode-hook 'my-prog-mode-hook)
 
-;; git thingies
-(defhook git-commit-mode-hook
-  (flyspell-mode 't))
+;; things like markdown
+(defhook text-mode-hook
+  (flyspell-mode 't)
+
+  (lsk 'langtool-check-buffer "C-c C-l")
+  (lsk 'langtool-show-message-at-point "C-c C-p")
+  (lsk 'langtool-goto-previous-error "<f7>")
+  (lsk 'langtool-goto-next-error "<f8>"))
+
+;; git commits are text too.
+(add-hook 'git-commit-mode-hook 'my-text-mode-hook)
 
 ;; eww thingies
 (defhook eww-mode-hook
