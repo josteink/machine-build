21647b171c9ce46fcb6cd5febe2d34150c260433
Jostein Kjønigsen <jostein@kjonigsen.net>
2016-07-29 09:26:54 +0200

    Emacs: use helm for find-file.

--- dotfiles/emacs/dot-emacs.el
+++ dotfiles/emacs/dot-emacs.el
@@ -296,6 +296,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 ;; (global-set-key (kbd "C-c p h") 'helm-projectile)
 
 ;; always follow symlinks to files under source-control. dont ask.
