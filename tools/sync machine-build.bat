7dc5cdc22b8b15f21cb65f43083737c51eb2cd4b
Jostein Kjønigsen <jostein@kjonigsen.net>
2016-09-27 10:13:21 +0200

    Simplified config-synching
    No need to bootstrap cygwin git when we have proper .gitattributes.

--- tools/sync machine-build.bat
+++ tools/sync machine-build.bat
@@ -1,15 +1,25 @@
 @echo off
-set GITROOT=/cygdrive/d/Git/machine-build
 
-REM two remarks about this invocation:
-REM
-REM 1. we need to be explicit about WHICH bash we're launching, or we might
-REM    launch msysgit's bash. This does not do what we want.
-REM
-REM 2. we need to invoke our scripts through a interactive logged in bash
-REM    otherwise we will not be in "cygwin"-mode and git will have issues with
-REM    file-permissions and think we're in a dirty state.
-REM
-REM With that out of the way, pass on control to the script which does the
-REM actual work.
-C:\cygwin64\bin\bash.exe --login -i "%GITROOT%/tools/sync machine-build.sh"
+echo Synching machine settings from git origin.
+
+echo Script drive:
+echo %~d0
+%~d0
+
+echo Script dir:
+echo %~dp0
+cd %~dp0
+
+echo Git root:
+cd ..
+cd
+
+echo Remote:
+git remote -v
+
+echo Synching settings...
+git pull --rebase
+
+
+echo Done.
+pause >nul
--- tools/sync machine-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# get script-folder, without trailing /
-CURRENT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT="$CURRENT/.."
-
-cd $ROOT
-git pull
