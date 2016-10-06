8e1abca1400993369503c41d9e2d866422b5d993
Jostein Kjønigsen <jostein@kjonigsen.net>
2016-10-06 14:11:36 +0200

    Add seamless SOS loading.

--- dotfiles/emacs/cdb-comint-mode.el
+++ dotfiles/emacs/cdb-comint-mode.el
@@ -108,6 +108,20 @@
       (comint-send-input))))
 
 
+;; utility functions
+
+(defun cdb-send-command (string)
+  (let ((buffer (get-buffer-create "*cdb*")))
+    (comint-send-string (get-buffer-process buffer)
+                        (concat string "\n"))))
+
+(defun cdb-load-sos ()
+  (interactive)
+  (cdb-send-command ".sympath %temp%\symbolcache")
+  (cdb-send-command ".symfix+")
+  (cdb-send-command ".cordll -ve -u -l")
+  (cdb-send-command ".reload"))
+
 ;; superoffice-specific
 (defun my-cdb-custom-highlight-function ()
   ;; Some things should light up subtly in log
