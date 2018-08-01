a3614cf4752dbcbb75f896830c12e0659897a20a
Jostein Kjønigsen <jostein@kjonigsen.net>
2018-08-01 20:33:17 +0200

    package.el fixes.

--- dotfiles/emacs/dot-emacs.el
+++ dotfiles/emacs/dot-emacs.el
@@ -1,6 +1,13 @@
 
 ;;;; Custom
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
 (when (file-exists-p custom-file)
   (load-file custom-file))
