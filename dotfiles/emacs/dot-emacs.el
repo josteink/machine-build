03a8ea0c87ee617447404fdf109e343cdffb4363
Jostein Kjønigsen <jostein@kjonigsen.net>
2018-08-01 20:37:38 +0200

    Go away package.el things.

--- dotfiles/emacs/dot-emacs.el
+++ dotfiles/emacs/dot-emacs.el
@@ -1,13 +1,6 @@
 
 ;;;; Custom
 
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
 (when (file-exists-p custom-file)
   (load-file custom-file))
@@ -35,7 +28,6 @@
 
 
 (add-to-list 'package-archives '("org-mode"     . "http://orgmode.org/elpa/"))
-(package-initialize)
 
 ;; ensure all packages we need are installed.
 (setq my-packages
