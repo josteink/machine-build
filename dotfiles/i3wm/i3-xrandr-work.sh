5a02e3b928b13f5b364b78e0225a5f503196e452
Jostein Kjønigsen <jostein@kjonigsen.net>
2020-01-27 07:37:29 +0100

    More X11 output setting tweaks.

--- dotfiles/i3wm/i3-xrandr-work.sh
+++ dotfiles/i3wm/i3-xrandr-work.sh
@@ -3,3 +3,5 @@
 xrandr --output DP-2 --auto --left-of DP-1  --rotate left
 xrandr --output DP-3 --auto --right-of DP-1 --rotate left
 
+# position middle screen at 600 pixels down, compared to others screens.
+xrandr --output DP-1 --panning 2560x1440+1440+600
