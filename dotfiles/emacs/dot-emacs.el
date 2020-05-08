110f55c86196dedfaae3445b2fc75ed1f33dcc63
Jostein Kjønigsen <jostein@kjonigsen.net>
2020-05-08 08:44:14 +0200

    Emacs: remember to enable editor-config, not just install it

--- dotfiles/emacs/dot-emacs.el
+++ dotfiles/emacs/dot-emacs.el
@@ -1672,7 +1672,9 @@ Searches for last face, or new face if invoked with prefix-argument"
   (when (not (or (and (derived-mode-p 'csharp-mode) (not (eq my-csharp-backend 'lsp)))
                  (and (derived-mode-p 'typescript-mode) (not (eq my-typescript-backend 'lsp)))
               ))
-    (lsp)))
+    (lsp))
+
+  (editorconfig-mode t))
 
 (add-hook 'powershell-mode-hook 'my-prog-mode-hook)
 (add-hook 'css-mode-hook 'my-prog-mode-hook)
