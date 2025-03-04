d4283cb3bb60dd4923ce74d313155d5e4a1dc8ad
Jostein Kjønigsen <jostein@kjonigsen.net>
2025-03-04 09:21:33 +0100

    Emacs: Make the minibuffer (and grep mode!) even more awesome!

--- dotfiles/emacs/dot-emacs.el
+++ dotfiles/emacs/dot-emacs.el
@@ -211,6 +211,22 @@
 ;; (setq orderless-matching-styles '(orderless-flex))
 (setq orderless-matching-styles '(orderless-literal))
 
+;; allow us to export consult-grep etc to its own grep-buffer
+(use-package embark
+  :ensure t
+  :bind (("C-." . embark-act)
+         :map minibuffer-local-map
+         ("C-c C-c" . embark-collect)
+         ("C-c C-e" . embark-export)))
+(use-package embark-consult
+  :ensure t)
+
+(use-package wgrep
+  :ensure t
+  :bind ( :map grep-mode-map
+          ("e" . wgrep-change-to-wgrep-mode)
+          ("C-x C-q" . wgrep-change-to-wgrep-mode)
+          ("C-c C-c" . wgrep-finish-edit)))
 
 (use-package magit
   :ensure t
